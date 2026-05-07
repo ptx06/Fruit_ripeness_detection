9a3a98063ae8b59533089743d2a083e5cf1b4f09
git_ftx <304941600@qq.com>
2026-05-07 21:48:39 +0800

    v3

--- src/fruit_detection/data/__init__.py
+++ src/fruit_detection/data/__init__.py
@@ -1,4 +1,4 @@
-"""数据处理模块"""
-from .dataset import FruitDataset
+"""数据处理模块 - 同时支持目标检测和图片分类"""
+from .dataset import DetectionDataset, ClassificationDataset, create_classification_data_loaders
 
-__all__ = ["FruitDataset"]
+__all__ = ["DetectionDataset", "ClassificationDataset", "create_classification_data_loaders"]
